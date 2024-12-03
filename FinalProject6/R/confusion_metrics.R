17f87cd5c5962f919a8cf0bfede636f5e49c893c
jtp579 <jacobpennington579@gmail.com>
2024-12-03 16:20:25 -0600

    Added help documentation for all functions

---
+++ FinalProject6/R/confusion_metrics.R
@@ -0,0 +1,66 @@
+#' @title Confusion Metrics
+#'
+#' @description Given coefficient vector beta, feature data, and classifications, output the
+#' predictions of logistic regression with the correct classifications in a confusion matrix.
+#' Also output prevalence, accuracy, sensitivity, specificity, false discovery rate, and
+#' diagnostic odds ratio.
+#' @param beta A \code{vector} of size $p$ x $1$, where p is the number of features in the data
+#' (including the intercept), representing the coefficients we will use to predict with logistic
+#' regression.
+#' @param x A \code{matrix} of size $n$ x $p$, where n is the number of observations of the data
+#' and $p$ is the number of features (including the intercept).
+#' @param y A \code{vector} of size $n$ x $1$ representing the classification of each of the $n$
+#' observations, 0 or 1.
+#' @return A \code{list} containing the following attributes:
+#' \describe{
+#' \item{ConfusionMatrix}{A \code{matrix} of integers that represent how many of each classification
+#' logistic regression classified correctly and incorrectly using the provided beta vector}
+#' \item{Metrics}{A \code{list} of six numeric values which are, in order:}
+#'      \describe{
+#'           \item{Prevalence}
+#'           \item{Accuracy}
+#'           \item{Sensitivity}
+#'           \item{Specificity}
+#'           \item{FalseDiscoveryRate}
+#'           \item{DiagnosticOddsRatio}
+#'      }
+#'    }
+#' }
+#' @author Shuqi Du
+#' @export
+confusion_metrics <- function(beta, x, y) {
+  # Calculate predicted probabilities
+  p <- 1 / (1 + exp(-x %*% beta))
+
+  # Convert probabilities to binary predictions using a cutoff of 0.5
+  y_pred <- ifelse(p > 0.5, 1, 0)
+
+  # Create confusion matrix
+  tp <- sum(y_pred == 1 & y == 1)
+  tn <- sum(y_pred == 0 & y == 0)
+  fp <- sum(y_pred == 1 & y == 0)
+  fn <- sum(y_pred == 0 & y == 1)
+
+  confusion_matrix <- matrix(c(tp, fp, fn, tn), nrow = 2, byrow = TRUE)
+  rownames(confusion_matrix) <- c("Predicted 1", "Predicted 0")
+  colnames(confusion_matrix) <- c("Actual 1", "Actual 0")
+
+  # Calculate metrics
+  prevalence <- (tp + fn) / (tp + tn + fp + fn)
+  accuracy <- (tp + tn) / (tp + tn + fp + fn)
+  sensitivity <- tp / (tp + fn)
+  specificity <- tn / (tn + fp)
+  false_discovery_rate <- fp / (tp + fp)
+  diagnostic_odds_ratio <- (tp / fn) / (fp / tn)
+
+  metrics <- list(
+    Prevalence = prevalence,
+    Accuracy = accuracy,
+    Sensitivity = sensitivity,
+    Specificity = specificity,
+    FalseDiscoveryRate = false_discovery_rate,
+    DiagnosticOddsRatio = diagnostic_odds_ratio
+  )
+
+  return(list(ConfusionMatrix = confusion_matrix, Metrics = metrics))
+}
---
+++ FinalProject6/R/logistic_regression.R
@@ -0,0 +1,64 @@
+#' @title Logistic Regression
+#'
+#' @description Given input data, run logistic regression such that the loss
+#' function specified in the instructions is minimized and return data trained on,
+#' list of feature names, and initial and optimal beta vectors.
+#' @param x A \code{character vector} containing the names of all features
+#' we will train the binary classification on.
+#' @param y A \code{character} containing the name of the classified feature
+#' (which we will fit the model on).
+#' @param data A \code{data frame} containing the data we will train the binary
+#' classification on.
+#' @return A \code{list} containing these two lists, where $n$ is the number
+#' of observations in the given data frame and $k$ is the number of provided features
+#' to predict on:
+#' \describe{
+#' \item{user_data}{A \code{list} containing the following attributes:
+#' to predict on:
+#'      \describe{
+#'           \item{x}{An $n$ x $k+1$ \cod{matrix} containing the predictors we used to train}
+#'           \item{y}{An $n$ x $1$ \code{vector} containing the classification of each observation}
+#'         }
+#'    }
+#' \item{coefficients}{A \code{list} containing the following attributes:
+#'      \describe{
+#'           \item{Coefficient}{A \code{character vector} of length $k+1$ containing the names
+#'           of all features logistic regression was trained on (including intercept)}
+#'           \item{beta_initial}{A $k+1$ x $1$ \code{vector} which is the initial coefficient
+#'           values for optimization using the least squares formula}
+#'           \item{beta_estimate}{A $k+1$ x $1$ \code{vector} which is the optimal beta hat
+#'           coefficient vector calculated by optimizing the provided loss function}
+#'      }
+#'    }
+#' }
+#' @author Emily Knight
+#' @export
+#' @examples
+#' logistic_regression(x = c("age", "workclass", "house.per.week"), y = "sex", data = adult)
+logistic_regression <- function(x, y, data){
+
+  list1 <- format_input(x = x, y = y, data = data)
+
+  # Determine Initial Beta Coefficients
+  beta_initial <- beta_initial_guess(x, y, data)
+
+  # set initial epsilon_applied to FALSE for use in generating a warning if used in the loss function
+  epsilon_applied <<- FALSE
+
+  # estimate coefficients
+  optimal_result <- optim(par = beta_initial,
+                          fn = loss_function,
+                          x = list1$x,
+                          y = list1$y,
+                          method = "BFGS")
+
+  beta_estimate <- optimal_result$par
+
+  # update list
+  list2 <- list(Coefficient = colnames(list1$x), beta_initial = beta_initial,  beta_estimate = beta_estimate)
+  my_output <- list(user_data = list1, coefficients = list2)
+
+  # provide user with relevant output
+  print(my_output$coefficients)
+  return(my_output)
+}
